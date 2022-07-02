50c6420160a625ab00ce12cb8fb903e4af2051e7
dennisooki <dennisooki@gmail.com>
2022-07-02 07:36:55 +0300

    first commit

--- assets/index.js
+++ assets/index.js
@@ -6,13 +6,42 @@ const wrapper = document.querySelector(".wrapper"),
   infoText = wrapper.querySelector(".info-text");
 let audio;
 
-// fetch api function
-function fetchApi(word) {
-  infoText.style.color = "#000";
-  wrapper.classList.remove("active");
-  infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
-  let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((result) => data(result, word));
+function data(result, word){
+    wrapper.classList.add("active");
+
+    let definitions = result[0].meanings[0].definitions[0];
+    let phontetics = `${result[0].meanings[0].partOfSpeech}  /${result[0].phonetics[0].text}/`;
+    document.querySelector(".word p").innerText = result[0].word;
+    document.querySelector(".word span").innerText = phontetics;
+    document.querySelector(".meaning span").innerText = definitions.definition;
+    document.querySelector(".example span").innerText = definitions.example;
+
+    if(definitions.synonyms[0] == undefined){
+        synonyms.parentElement.style.display = "none";
+    }
+
 }
+
+function search(word){
+    fetchApi(word);
+    searchInput.value = word;
+}
+
+function fetchApi(word){
+    wrapper.classList.remove("active");
+    infoText.style.color = "#000";
+    infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    fetch(url).then(response => response.json()).then(result => data(result, word)).catch(() =>{
+        infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
+    });
+}
+
+searchInput.addEventListener("keyup", e =>{
+    let word = e.target.value
+    if(e.key == "Enter" && word){
+        fetchApi(word);
+    }
+});
+
+
